d30e9865b0cacb4457d8f9a6df91845d233c688d
Evan <thueeliyifan@gmail.com>
2015-04-12 20:39:10 -0400

    architect the loadbalancer

---
+++ SolrCloudSimulator/src/edu/cmu/ece845/loadbalancer/IndexServer.java
@@ -0,0 +1,15 @@
+package edu.cmu.ece845.loadbalancer;
+/*
+ * @Author: Yifan Li
+ * @Description: This class will handle the index requests from the client.
+ * @Date: April 12, 2015
+ */
+public class IndexServer implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
